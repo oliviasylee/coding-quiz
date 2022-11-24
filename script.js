002b9ca470fdc9a1d1bb5a70e381ab0b29a43889
Olivia Lee <115487208+oliviasylee@users.noreply.github.com>
2022-11-23 22:08:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -147,21 +147,14 @@ function quizOver() {
 function saveHighscore() {
   var initials = initialsEl.value.trim();
   if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
     var highscores =
       JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-    // format new score object for current user
     var newScore = {
       score: time,
       initials: initials
     };
-
-    // save to localstorage
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
-
-    // redirect to next page
     window.location.href = "view-highscore.html";
   }
 }
